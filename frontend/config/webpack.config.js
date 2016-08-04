3868635fe6a63b7953656db954a85a1e9269f962
akichim21 <afujiwara2012@gmail.com>
2016-08-04 13:04:14 +0000

    master add frontend .eslintrc.json & webpack.config.js & package.json

---
+++ frontend/config/webpack.config.js
@@ -0,0 +1,41 @@
+const DEBUG = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined;
+
+module.exports = {
+  cache: DEBUG,
+  debug: DEBUG,
+  devtool: DEBUG ? 'inline-source-map' : false,
+  entry: {
+  },
+  output: {
+    path: '../app/assets/javascripts/components',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel?presets[]=react,presets[]=es2015,presets[]=stage-2'
+      },
+      { test: /\.json$/, loader: 'json' }
+    ],
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ]
+  },
+  eslint: {
+    configFile: './.eslintrc.json'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', 'json'],
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
+  }
+}
+
