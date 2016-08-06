495c277e1064bbbc6769eca73391af8d491176c7
akichim21 <afujiwara2012@gmail.com>
2016-08-06 01:05:21 +0000

    master add todo model

---
+++ db/migrate/20160806010121_create_todos.rb
@@ -0,0 +1,10 @@
+class CreateTodos < ActiveRecord::Migration[5.0]
+  def change
+    create_table :todos do |t|
+      t.string :text
+      t.boolean :is_complete
+
+      t.timestamps
+    end
+  end
+end
